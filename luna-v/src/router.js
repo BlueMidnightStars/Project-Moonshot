634234518c3532e298ecf31be408ef7e68a5a103
涛 <tao@taodeMacBook-Pro.local>
2019-02-25 15:37:24 +0800

    v

---
+++ luna-v/src/router.js
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import greet from './views/greet.vue';
+import enter from './views/enter.vue';
+
+
+
+
+
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/greet',
+      name: 'greet',
+      component: greet,
+    },
+    {
+      path: '/enter',
+      name: 'enter',
+      component: enter,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+  ],
+});
